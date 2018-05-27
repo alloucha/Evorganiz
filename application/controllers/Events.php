a891b9e5d83fa2180bb2b1a730694ceb9f27af9a
aliachawaf <aliachawaf@gmail.com>
2018-05-27 02:33:02 +0200

    MODIF view events

---
+++ application/controllers/Events.php
@@ -0,0 +1,35 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Events extends CI_Controller {
+
+	public function __construct()
+	{
+	  parent::__construct();
+	  $this->load->model('Event/ListEvents');
+	}
+
+	public function index() {
+		$this->ListEvent();
+	}
+
+	public function ListEvent() {
+
+		$data['page'] = $this->tableauEvent();
+
+		$data['title']= 'EVENEMENT';
+		$this->load->view("Theme/theme", $data);
+
+
+	}
+
+	public function tableauEvent() {
+
+		$data['ListEvents']= $this->ListEvents->getEvents();
+
+		return $this->load->view("Event/ListEvents", $data, true);
+	}
+
+
+	
+}
--- application/views/Event/ListEvents.php
+++ application/views/Event/ListEvents.php
@@ -12,79 +12,96 @@
                     </div>
                 </div>
             </div><!-- /.box-header -->
-            <div class="box-body table-responsive no-padding">
-                <table class="table table-striped">
-                    <tbody><tr>
-                        <th>n°</th>
-                        <th>Date</th>
-                        <th>Occasion</th>
-                        <th>Personne concernée</th>
-                        <th>Thème</th>
-                        <th>Lieu</th>
-                        <th>Nb d'invités</th>
-                        <th>Budget Max</th>
-                    </tr>
-                    
-                    <!--
-                    <tr>
-                        <td>183</td>
-                        <td>John Doe</td>
-                        <td>11-7-2014</td>
-                        <td><span class="label label-success">Approved</span></td>
-                        <td>Bacon ipsum dolor sit amet salami venison chicken flank fatback doner.</td>
-                        <td>John Doe</td>
-                        <td>11-7-2014</td>
-                    </tr>
-                    -->
-
-            <?php
-            
-                // Data recovery
-                $line = '';
-                foreach ($ListEvents as $event) {
-                    $idEvent = $event->idEvent;
-                    $dateEvent = $event->dateEvent;
-                    $occasionEvent = $event->occasionEvent;
-                    $personConcerned = $event->personConcerned;
-                    $themeEvent = $event->themeEvent;
-                    $venueEvent = $event->venueEvent;
-                    $budgetMaxEvent = $event->budgetMaxEvent;           
-                    
-                    // for each loop iteration, a line contains all informations about one event
-                    $ligne = '<tr>';
-                    //$ligne = $ligne . '<td><a  href="' . site_url('ficheEditeur?idFicheEditeur='. $idEditeur ) . '" >' . $nomEditeur . '</a></td>';
-                    
-                    //$ligne = $ligne . '<td><a  href="contact">' . $nomContact . '</a></td>';
-                    $ligne = $ligne . '<td>' . $idEvent . '</td>';
-                    $ligne = $ligne . '<td>' . $dateEvent . '</td>';
-                    $ligne = $ligne . '<td>' . $occasionEvent . '</td>';
-                    $ligne = $ligne . '<td>' . $personConcerned . '</td>';
-                    $ligne = $ligne . '<td>' . $themeEvent . '</td>';
-                    $ligne = $ligne . '<td>' . $venueEvent . '</td>';
-                    $ligne = $ligne . '<td>' . $budgetMaxEvent . '</td>';
-                    $ligne = $ligne . '<td>' . $budgetMaxEvent . '</td>';
-
-                    // Add a button to delete or edit an event
-    //                    $ligne = $ligne . '<td class="row">';
-
-                    ////////to do buttton 
-
-                    $ligne = $ligne . '</tr>';
-                    
-                    echo  $ligne;
-                    
-                }
-                    
 
 
-                ?>
+            <div class="box-body table-responsive">
+                <div id="example1_wrapper" class="dataTables_wrapper form-inline" role="grid">
+                    <div class="row"><div class="col-xs-6"><div id="example1_length" class="dataTables_length">
+                        <label>
+                        <select size="1" name="example1_length" aria-controls="example1">
+                            <option value="10" selected="selected">10</option><option value="25">25</option>
+                            <option value="50">50</option><option value="100">100</option>
+                        </select> records per page</label>
 
+
+                    </div>
+                </div>
+
+            </div>
+
+            <table id="example1" class="table table-bordered table-striped dataTable" aria-describedby="example1_info">
+                <thead><tr role="row">
+
+                    <th class="sorting_asc" role="columnheader" tabindex="0" aria-controls="example1" rowspan="1" colspan="1" aria-sort="ascending" aria-label="Rendering engine: activate to sort column descending" style="width: 212px;">n°</th>
+
+                    <th class="sorting" role="columnheader" tabindex="0" aria-controls="example1" rowspan="1" colspan="1" aria-label="Browser: activate to sort column ascending" >Date</th>
+
+                    <th class="sorting" role="columnheader" tabindex="0" aria-controls="example1" rowspan="1" colspan="1" aria-label="Platform(s): activate to sort column ascending">Occasion</th>
+
+                    <th class="sorting" role="columnheader" tabindex="0" aria-controls="example1" rowspan="1" colspan="1" aria-label="Engine version: activate to sort column ascending">Personne concernée</th>
+
+                    <th class="sorting" role="columnheader" tabindex="0" aria-controls="example1" rowspan="1" colspan="1" aria-label="CSS grade: activate to sort column ascending">Thème</th>
+
+                    <th class="sorting" role="columnheader" tabindex="0" aria-controls="example1" rowspan="1" colspan="1" aria-label="CSS grade: activate to sort column ascending">Lieu</th>
+
+                    <th class="sorting" role="columnheader" tabindex="0" aria-controls="example1" rowspan="1" colspan="1" aria-label="CSS grade: activate to sort column ascending">Nb d'invités</th>
+
+                    <th class="sorting" role="columnheader" tabindex="0" aria-controls="example1" rowspan="1" colspan="1" aria-label="CSS grade: activate to sort column ascending">Budget Max</th>
+
+                </tr></thead>
+    
                 
+                <tbody role="alert" aria-live="polite" aria-relevant="all">
 
-                </tbody></table>
+                    <?php
 
-                <div class="box-footer clearfix">
-                    <ul class="pagination pagination-sm no-margin pull-right">
+                        // Data recovery
+                        $line = '';
+                        foreach ($ListEvents as $event) {
+                            $idEvent = $event->idEvent;
+                            $dateEvent = $event->dateEvent;
+                            $occasionEvent = $event->occasionEvent;
+                            $personConcerned = $event->personConcerned;
+                            $themeEvent = $event->themeEvent;
+                            $venueEvent = $event->venueEvent;
+                            $budgetMaxEvent = $event->budgetMaxEvent;           
+                            
+                            // for each loop iteration, a line contains all informations about one event
+                            $ligne = '<tr>';
+                            //$ligne = $ligne . '<td><a  href="' . site_url('ficheEditeur?idFicheEditeur='. $idEditeur ) . '" >' . $nomEditeur . '</a></td>';
+                            
+                            //$ligne = $ligne . '<td><a  href="contact">' . $nomContact . '</a></td>';
+                            $ligne = $ligne . '<td>' . $idEvent . '</td>';
+                            $ligne = $ligne . '<td>' . $dateEvent . '</td>';
+                            $ligne = $ligne . '<td>' . $occasionEvent . '</td>';
+                            $ligne = $ligne . '<td>' . $personConcerned . '</td>';
+                            $ligne = $ligne . '<td>' . $themeEvent . '</td>';
+                            $ligne = $ligne . '<td>' . $venueEvent . '</td>';
+                            $ligne = $ligne . '<td>' . $budgetMaxEvent . '</td>';
+                            $ligne = $ligne . '<td>' . $budgetMaxEvent . '</td>';
+
+                            // Add a button to delete or edit an event
+                            // $ligne = $ligne . '<td class="row">';
+
+                            ////////to do buttton 
+
+                            $ligne = $ligne . '</tr>';
+                            
+                            echo  $ligne;
+                            
+                        }
+
+                    ?>
+                </tbody>
+            </table>
+
+            <div class="row">
+                <div class="col-xs-6">
+                    <div class="dataTables_info" id="example1_info">Showing 1 to 10 of 57 entries</div>
+                </div>
+
+                <div class="col-xs-6">
+                    <ul class="pagination pagination-sm margin pull-right">
                         <li><a href="#">«</a></li>
                         <li><a href="#">1</a></li>
                         <li><a href="#">2</a></li>
@@ -92,8 +109,7 @@
                         <li><a href="#">»</a></li>
                     </ul>
                 </div>
-
-            </div><!-- /.box-body -->
-        </div><!-- /.box -->
-    </div>
+            </div>
+        </div><!-- /.box-body -->
+    </div><!-- /.box -->
 </div>
