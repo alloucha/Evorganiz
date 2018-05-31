96960f0d7d9a887d9a9172060b894ce87bb0e055
aliachawaf <aliachawaf@gmail.com>
2018-05-31 20:13:14 +0200

    ADD encryption key

--- application/config/config.php
+++ application/config/config.php
@@ -325,7 +325,7 @@ $config['cache_query_string'] = FALSE;
 | https://codeigniter.com/user_guide/libraries/encryption.html
 |
 */
-$config['encryption_key'] = '';
+$config['encryption_key'] = 'd5h8ùMPà6f5A5*KM';
 
 /*
 |--------------------------------------------------------------------------
