6c84606133d084c8787a9d7536a2f8f95f36027e
aliachawaf <aliachawaf@gmail.com>
2018-06-01 22:52:25 +0200

    MODIF scripts theme

--- application/views/Theme/theme.php
+++ application/views/Theme/theme.php
@@ -5,33 +5,35 @@
         <title><?php echo $title?></title>
         <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
         <!-- bootstrap 3.0.2 -->
-        <link href="<?php echo base_url() ?>AdminLTE/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+        <link href="../AdminLTE/css/bootstrap.min.css" rel="stylesheet" type="text/css">
         <!-- font Awesome -->
-        <link href="<?php echo base_url() ?>AdminLTE/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+        <link href="../AdminLTE/css/font-awesome.min.css" rel="stylesheet" type="text/css">
         <!-- Ionicons -->
-        <link href="<?php echo base_url() ?>AdminLTE/css/ionicons.min.css" rel="stylesheet" type="text/css" />
+        <link href="../AdminLTE/css/ionicons.min.css" rel="stylesheet" type="text/css">
         <!-- Morris chart -->
-        <link href="<?php echo base_url() ?>AdminLTE/css/morris/morris.css" rel="stylesheet" type="text/css" />
+        <link href="../AdminLTE/css/morris/morris.css" rel="stylesheet" type="text/css" />
         <!-- jvectormap -->
-        <link href="<?php echo base_url() ?>AdminLTE/css/jvectormap/jquery-jvectormap-1.2.2.css" rel="stylesheet" type="text/css" />
+        <link href="../AdminLTE/css/jvectormap/jquery-jvectormap-1.2.2.css" rel="stylesheet" type="text/css" />
         <!-- fullCalendar -->
-        <link href="<?php echo base_url() ?>AdminLTE/css/fullcalendar/fullcalendar.css" rel="stylesheet" type="text/css" />
+        <link href="../AdminLTE/css/fullcalendar/fullcalendar.css" rel="stylesheet" type="text/css" />
         <!-- Daterange picker -->
-        <link href="<?php echo base_url() ?>AdminLTE/css/daterangepicker/daterangepicker-bs3.css" rel="stylesheet" type="text/css" />
+        <link href="../AdminLTE/css/daterangepicker/daterangepicker-bs3.css" rel="stylesheet" type="text/css" />
         <!-- bootstrap wysihtml5 - text editor -->
-        <link href="<?php echo base_url() ?>AdminLTE/css/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css" rel="stylesheet" type="text/css" />
+        <link href="../AdminLTE/css/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css" rel="stylesheet" type="text/css" />
         <!-- Theme style -->
-        <link href="<?php echo base_url() ?>AdminLTE/css/AdminLTE.css" rel="stylesheet" type="text/css" />
+        <link href="../AdminLTE/css/AdminLTE.css" rel="stylesheet" type="text/css" />
 
         <link rel="icon" href="<?php echo base_url() ?>AdminLTE/logo.png">
 
+        <!-- jQuery UI 1.10.3 -->
+        <script src="<?php echo base_url() ?>AdminLTE/js/jquery-ui-1.10.3.min.js" type="text/javascript"></script>
 
-        <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-        <!--[if lt IE 9]>
-          <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-          <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
-        <![endif]-->
+        <!-- jQuery 2.0.2 -->
+        <script src="<?php echo base_url() ?>AdminLTE/js/jquery.min.js"></script>
+
+
+
+
     </head>
     <body class="skin-black">
         <!-- header logo: style can be found in header.less -->
@@ -398,27 +400,10 @@
         </div><!-- ./wrapper -->
 
         
-
-        <!-- add new calendar event modal -->
-
-        <!-- jQuery 2.0.2 -->
-        <script src="<?php echo base_url() ?>AdminLTE/js/jquery.min.js"></script>
-        <!-- jQuery UI 1.10.3 -->
-        <script src="<?php echo base_url() ?>AdminLTE/js/jquery-ui-1.10.3.min.js" type="text/javascript"></script>
         <!-- Bootstrap -->
         <script src="<?php echo base_url() ?>AdminLTE/js/bootstrap.min.js" type="text/javascript"></script>
-        <!-- Morris.js charts -->
-        <script src="//cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script>
-        <script src="<?php echo base_url() ?>AdminLTE/js/plugins/morris/morris.min.js" type="text/javascript"></script>
-        <!-- Sparkline -->
-        <script src="<?php echo base_url() ?>AdminLTE/js/plugins/sparkline/jquery.sparkline.min.js" type="text/javascript"></script>
-        <!-- jvectormap -->
-        <script src="<?php echo base_url() ?>AdminLTE/js/plugins/jvectormap/jquery-jvectormap-1.2.2.min.js" type="text/javascript"></script>
-        <script src="<?php echo base_url() ?>AdminLTE/js/plugins/jvectormap/jquery-jvectormap-world-mill-en.js" type="text/javascript"></script>
-        <!-- fullCalendar -->
         <script src="<?php echo base_url() ?>AdminLTE/js/plugins/fullcalendar/fullcalendar.min.js" type="text/javascript"></script>
-        <!-- jQuery Knob Chart -->
-        <script src="<?php echo base_url() ?>AdminLTE/js/plugins/jqueryKnob/jquery.knob.js" type="text/javascript"></script>
+        
         <!-- daterangepicker -->
         <script src="<?php echo base_url() ?>AdminLTE/js/plugins/daterangepicker/daterangepicker.js" type="text/javascript"></script>
         <!-- Bootstrap WYSIHTML5 -->
