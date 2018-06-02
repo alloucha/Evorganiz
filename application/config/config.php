fe64b9dd75f49a699775df2d5b01c81d933dcb76
aliachawaf <aliachawaf@gmail.com>
2018-06-02 15:59:11 +0200

    FEAT remove index.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/Evorganiz';
+$config['base_url'] = 'http://localhost/Evorganiz/';
 //https://evorganiz.herokuapp.com/
 
 /*
@@ -36,7 +36,7 @@ $config['base_url'] = 'http://localhost/Evorganiz';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
