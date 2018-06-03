6b74e5f65e151eb441f072ae31df4750f64eeba8
aliachawaf <aliachawaf@gmail.com>
2018-06-03 12:03:09 +0200

    return in local

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'https://evorganiz.herokuapp.com/';
+$config['base_url'] = 'https://localhost/Evorganiz/';
+
+//https://evorganiz.herokuapp.com/
 
 
 /*
