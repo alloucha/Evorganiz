52f87a723796d442f8af921bbc6f9e63c857b724
aliachawaf <aliachawaf@gmail.com>
2018-06-03 16:44:19 +0200

    FIX purchase

--- application/controllers/Purchase.php
+++ application/controllers/Purchase.php
@@ -11,12 +11,5 @@ class Purchase extends CI_Controller {
 	public function index() {
 
 		$data['page'] = $this->load->view("Purchase/Purchase_view");
-
-		$idUser = get_cookie('idUserCookie');
-		$idUser = $this->encryption->decrypt($idUser);
-		$userInfo = $this->User_model->getUserByIdUser;
-		$data['userInfo'] = $userInfo;
-		$data['title']= 'ACHAT';
-		$this->load->view("Theme/theme", $data);
 	}
 }
