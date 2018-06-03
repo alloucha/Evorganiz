684d47ece6ccc6e162dabeda116933b03c5140cb
aliachawaf <aliachawaf@gmail.com>
2018-06-03 06:31:35 +0200

    connect to heroku

--- application/config/database.php
+++ application/config/database.php
@@ -74,10 +74,7 @@ $active_group = 'default';
 $query_builder = TRUE;
 
 /*
-	'hostname' => 'ivgz2rnl5rh7sphb.chr7pe7iynqr.eu-west-1.rds.amazonaws.com',
-	'username' => 'm2k48m0iba6u19t2',
-	'password' => 'i0iw56t9vmf8n623',
-	'database' => 'p23o2g817jwn7i0q',
+		
 */
 
 $db['default'] = array(
